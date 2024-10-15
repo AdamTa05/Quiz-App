0c060f4917b826d1673b1fe4910c98715eb82bdc
AdamTa05 <adamtassaft05@gmail.com>
2024-10-16 01:23:45 +0200

    Implement views to handle quiz logic and user interactions

--- sim/views.py
+++ sim/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm          
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
